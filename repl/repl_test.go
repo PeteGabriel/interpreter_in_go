6a58bcf05573b7c73258c5db6442ff87057ea4f5
Pedro Gabriel <pedrogabriel@protonmail.ch>
2021-05-17 12:02:53 +0200

    Add unit test for REPL start method

--- repl/repl_test.go
+++ repl/repl_test.go
@@ -1,1 +1,19 @@
 package repl
+
+import (
+	"bytes"
+	is2 "github.com/matryer/is"
+	"testing"
+)
+
+func TestStart(t *testing.T) {
+	is := is2.New(t)
+	in := bytes.NewReader([]byte("10 + 10"))
+	var out bytes.Buffer
+	Start(in, &out)
+
+	vals := out.String()
+	exp := "{Type:INT Literal:10}\n{Type:+ Literal:+}\n{Type:INT Literal:10}\n"
+
+	is.True(vals == exp)
+}
